5f77eadcea163031c1e0b9d88ebbe2bb55154983
Kovacs, Balint <balint.kovacs@dkfz-heidelberg.de>
2023-11-28 17:30:50 +0100

    Cleanup

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer_AnatInf.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer_AnatInf.py
@@ -72,7 +72,6 @@ class nnUNetTrainer_AnatInf(nnUNetTrainer):
     def __init__(self, plans: dict, configuration: str, fold: int, dataset_json: dict, unpack_dataset: bool = True,
                  device: torch.device = torch.device('cuda')):
         super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
-        self.num_epochs = 3
 
     @staticmethod
     def build_network_architecture(plans_manager: PlansManager,
